573abc3c92ec50a927c539e0ff96ddd5519b14f0
MattT <main@MattTs-MacBook-Pro.local>
2023-01-13 21:37:51 -0500

    multiplayer backend start and transform straming almost working

---
+++ src/rooms/MyRoom.ts
@@ -0,0 +1,37 @@
+import { Room, Client } from "colyseus";
+import { MyRoomState, Player } from "./schema/MyRoomState";
+
+export class MyRoom extends Room<MyRoomState> {
+
+  onCreate (options: any) {
+    this.setState(new MyRoomState())
+
+    this.onMessage("transform_update", (client, message) => {
+      const player = this.state.players.get(client.sessionId);
+      player.transformData.pX = message.pX;
+      player.transformData.pY = message.pY;
+      player.transformData.pZ = message.pZ;
+      player.transformData.rX = message.rX;
+      player.transformData.rY = message.rY;
+      player.transformData.rZ = message.rZ;
+      player.transformData.rW = message.rW;
+    })
+  }
+
+  onJoin (client: Client, options: any) {
+    console.log(client.sessionId, "joined!")
+    const player = new Player()
+    this.state.players.set(client.sessionId, player)
+  }
+
+  onLeave (client: Client, consented: boolean) {
+    console.log(client.sessionId, "left!")
+    const player = this.state.players.get(client.sessionId)
+    this.state.players.delete(client.sessionId)
+  }
+
+  onDispose() {
+    console.log("room", this.roomId, "disposing...")
+  }
+
+}
---
+++ src/rooms/schema/MyRoomState.ts
@@ -0,0 +1,25 @@
+import { Schema, type, MapSchema } from "@colyseus/schema";
+
+
+class TransformData extends Schema {
+  @type("float32") rX: number = 0.0;
+  @type("float32") rY: number = 0.0;
+  @type("float32") rZ: number = 0.0;
+  @type("float32") rW: number = 0.0;
+  @type("float32") pX: number = 0.0;
+  @type("float32") pY: number = 1.6;
+  @type("float32") pZ: number = 0.0;
+}
+
+
+export class Player extends Schema {
+  @type(TransformData) transformData: TransformData = new TransformData()
+  @type("string") name: string = "Player";
+}
+
+
+export class MyRoomState extends Schema {
+
+  @type("string") mySynchronizedProperty: string = "Hello world";
+  @type({ map: Player }) players = new MapSchema<Player>();
+}
